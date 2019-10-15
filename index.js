96f5877e9fd87861725ad435efc384fb29d87a31
IB <iborkowskamail@gmail.com>
2019-10-15 16:56:17 +0200

    aquarium 3 poissons

---
+++ index.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+$('body').css({ backgroundColor: '#75e6e2' });
+const imgGoldfish = $('<img src="./assets/images/goldfish.svg" alt: goldfish id=goldfish />');
+imgGoldfish.css({ height: '4.5rem', width: '4.5rem' });
+$('body').append(imgGoldfish);
+
+const imgGoldfish2 = imgGoldfish.clone();
+imgGoldfish2.css({ transform: 'translateX(180px)rotate(-30deg)' });
+$('body').append(imgGoldfish2);
+
+const whale = $('<img src="./assets/images/whale.svg" alt: whale id=whale />');
+whale.appendTo('body');
+whale.css({ height: '15rem', width: '15rem' });
