bd9c5822e88bf7fb86c21463c7491d48852c5711
IB <iborkowskamail@gmail.com>
2019-10-23 16:46:17 +0200

    fishes changed the position without annimation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import $ from 'jquery';
 
-$('body').css({ backgroundColor: '#75e6e2' });
+$('body').css('backgroundColor', '#75e6e2');
 const imgGoldfish = $('<img src="./assets/images/goldfish.svg" alt: goldfish id=goldfish />');
-imgGoldfish.css({ height: '4.5rem', width: '4.5rem' });
+imgGoldfish.css({ height: '4.5rem', width: '4.5rem', transform: 'translate(500px,500px)' });
+
+
 $('body').append(imgGoldfish);
 
 const imgGoldfish2 = imgGoldfish.clone();
-imgGoldfish2.css({ transform: 'translateX(180px)rotate(-30deg)' });
+imgGoldfish2.css({ transform: 'translateX(600px)rotate(-30deg)' });
 $('body').append(imgGoldfish2);
 
 const whale = $('<img src="./assets/images/whale.svg" alt: whale id=whale />');
 whale.appendTo('body');
-whale.css({ height: '15rem', width: '15rem' });
+whale.css({ height: '15rem', width: '15rem', transform: 'translateX(300px)' });
